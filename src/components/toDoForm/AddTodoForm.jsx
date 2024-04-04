862914e542d3a864e83db5dcbfea417bb47a92b6
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-04-04 03:39:51 +0300

    deleted coments

--- src/components/toDoForm/AddTodoForm.jsx
+++ src/components/toDoForm/AddTodoForm.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// function AddTodoForm({ addTodo }) {
-//   return (
-//     <form
-//       className="add-todo-form"
-//       onSubmit={(e) => {
-//         e.preventDefault();
-//         const todoText = e.target.elements.todoText.value.trim();
-//         if (todoText) {
-//           addTodo(todoText);
-//           e.target.elements.todoText.value = "";
-//         }
-//       }}
-//     >
-//       <input type="text" name="todoText" />
-//       <button type="submit">Add Todo</button>
-//     </form>
-//   );
-// }
-
-// export default AddTodoForm;
-// TodoForm.js
-
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../../redux/actions";
--- src/components/toDoList/toDoList.jsx
+++ src/components/toDoList/toDoList.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-
-// function TodoList({ todos, toggleTodo }) {
-//   return (
-//     <ul className="todo-list">
-//       {todos.map((todo, index) => (
-//         <li
-//           key={index}
-//           className={`todo-item ${todo.completed ? "completed" : ""}`}
-//           onClick={() => toggleTodo(index)}
-//         >
-//           {todo.text}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-// export default TodoList;
-// TodoList.js
-
 import React from "react";
 import { connect } from "react-redux";
 import { toggleTodo } from "../../redux/actions";
