a39a5a81f264423987ca20dc12893eb668ec576f
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-04-04 03:57:37 +0300

    aded styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-// index.js
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import todoApp from "./redux/reducers";
 import App from "./App";
 
